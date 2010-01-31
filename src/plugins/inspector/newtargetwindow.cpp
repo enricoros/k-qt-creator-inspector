98a48e337a9d6ac2e5031431d1d5b86e277452ce
Enrico Ros <enrico.ros@gmail.com>
2010-01-31 15:51:14 +0100

    Inspector: layout of the main inspector screen.

--- src/plugins/inspector/newtargetwindow.cpp
+++ src/plugins/inspector/newtargetwindow.cpp
@@ -28,19 +28,211 @@
 **************************************************************************/
 
 #include "newtargetwindow.h"
-#include "ui_newtargetwindow.h"
+#include "inspectorplugin.h"
+#include "instance.h"
+#include "modulecontroller.h"
+#include <utils/stylehelper.h>
+#include <QtGui/QGridLayout>
+#include <QtGui/QIcon>
+#include <QtGui/QLabel>
+#include <QtGui/QPainter>
+#include <QtGui/QTableWidget>
 
-using namespace Inspector::Internal;
+namespace Inspector {
+namespace Internal {
+
+const int ICON_SIZE(64);
+const int ABOVE_HEADING_MARGIN(10);
+const int ABOVE_CONTENTS_MARGIN(4);
+const int BELOW_CONTENTS_MARGIN(16);
+const int LEFT_MARGIN(16);
+
+class OnePixelBlackLine : public QWidget
+{
+public:
+    OnePixelBlackLine(QWidget *parent)
+        : QWidget(parent)
+    {
+        setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        setMinimumHeight(1);
+        setMaximumHeight(1);
+    }
+    void paintEvent(QPaintEvent *e)
+    {
+        Q_UNUSED(e);
+        QPainter p(this);
+        p.fillRect(contentsRect(), QBrush(Utils::StyleHelper::borderColor()));
+    }
+};
+
 
 NewTargetWindow::NewTargetWindow(QWidget *parent)
-  : QWidget(parent)
-  , ui(new Ui::NewTargetWindow)
+  : QScrollArea(parent)
+  , m_root(new QWidget(this))
 {
-    ui->setupUi(this);
+    // We want a 900px wide widget with and the scrollbar at the side of the screen.
+    m_root->setFixedWidth(900);
+
+    m_layout = new QGridLayout(m_root);
+    m_layout->setColumnMinimumWidth(0, ICON_SIZE + 4);
+    m_layout->setSpacing(0);
+
+    setWidget(m_root);
+    setFrameStyle(QFrame::NoFrame);
+    setWidgetResizable(true);
+
+    // create the New Local Target widget
+    {
+        QWidget *widget = new QWidget;
+        QGridLayout *grid = new QGridLayout(widget);
+        grid->setMargin(0);
+        grid->setSpacing(0);
+        grid->setColumnMinimumWidth(0, LEFT_MARGIN);
+
+        QLabel *desc1 = new QLabel;
+        desc1->setText(tr("Select the kind of local target you want to Inspect."));
+        desc1->setWordWrap(true);
+        appendSubWidget(grid, desc1);
+
+        QWidget *runWidget = new QLabel("new options");
+        appendSubWidget(grid, runWidget, tr("New Run"),
+                        tr("Start a new instance of the selected project."));
+
+        QWidget *instWidget = new QLabel("running opts");
+        appendSubWidget(grid, instWidget, tr("Running Instances"),
+                        tr("Attach to an existing process."));
+
+        QWidget *debugWidget = new QLabel("debugging opts");
+        appendSubWidget(grid, debugWidget, tr("Debugging Instances"),
+                        tr("Use an existing debugging session."));
+
+        appendWrappedWidget(tr("New Local Target"),
+                            QIcon(":/inspector/images/inspector-icon-32.png"),
+                            widget);
+    }
+
+    // create the Active Targets widget
+    {
+        QWidget *widget = new QWidget;
+        QGridLayout *grid = new QGridLayout(widget);
+        grid->setMargin(0);
+        grid->setSpacing(0);
+        grid->setColumnMinimumWidth(0, LEFT_MARGIN);
+
+        QTableWidget *tw = new QTableWidget;
+        tw->setFrameStyle(QFrame::NoFrame);
+        tw->setFixedHeight(50);
+        appendSubWidget(grid, tw);
+
+        appendWrappedWidget(tr("Active Targets"),
+                            QIcon(":/projectexplorer/images/rebuild.png"),
+                            widget);
+    }
+
+    // create the Configure Frameworks widget
+    {
+        QWidget *widget = new QWidget;
+        QGridLayout *grid = new QGridLayout(widget);
+        grid->setMargin(0);
+        grid->setSpacing(0);
+        grid->setColumnMinimumWidth(0, LEFT_MARGIN);
+
+        QLabel *modules1 = new QLabel;
+        modules1->setWordWrap(true);
+        modules1->setText(tr("modules: %1\nrequires: %2").arg("some, hardcoded, strings, fix, this").arg("exclusive-probe"));
+//                InspectorPlugin::defaultInstance()->moduleController()->moduleNames()
+//                .join(", ")));
+
+        appendSubWidget(grid, modules1, "Qt");
+        appendSubWidget(grid, 0, "Qt-Custom#1");
+        appendSubWidget(grid, 0, "CUDA");
+        appendSubWidget(grid, 0, "OpenCL");
+
+        appendWrappedWidget(tr("Configure Frameworks"),
+                            QIcon(),
+                            widget);
+    }
 }
 
-NewTargetWindow::~NewTargetWindow()
+// keep this in sync with PanelsWidget::addPropertiesPanel in projectwindow.cpp
+void NewTargetWindow::appendWrappedWidget(const QString &title, const QIcon &icon,
+                                          QWidget *widget)
 {
-    delete ui;
+    // icon:
+    const int headerRow(m_layout->rowCount() - 1);
+    m_layout->setRowStretch(headerRow, 0);
+
+    if (!icon.isNull()) {
+        QLabel *iconLabel = new QLabel(m_root);
+        iconLabel->setPixmap(icon.pixmap(ICON_SIZE, ICON_SIZE));
+        iconLabel->setContentsMargins(0, ABOVE_HEADING_MARGIN, 0, 0);
+        m_layout->addWidget(iconLabel, headerRow, 0, 2, 1, Qt::AlignTop | Qt::AlignHCenter);
+    }
+
+    // name:
+    QLabel *nameLabel = new QLabel(m_root);
+    nameLabel->setText(title);
+    nameLabel->setContentsMargins(0, ABOVE_HEADING_MARGIN, 0, 0);
+    QFont f = nameLabel->font();
+    f.setBold(true);
+    f.setPointSizeF(f.pointSizeF() * 1.4);
+    nameLabel->setFont(f);
+    m_layout->addWidget(nameLabel, headerRow, 1, 1, 1, Qt::AlignBottom | Qt::AlignLeft);
+
+    // line:
+    const int lineRow(headerRow + 1);
+    m_layout->addWidget(new OnePixelBlackLine(m_root), lineRow, 1);
+
+    // add the widget:
+    const int widgetRow(lineRow + 1);
+    widget->setContentsMargins(m_layout->columnMinimumWidth(0),
+                               ABOVE_CONTENTS_MARGIN, 0,
+                               BELOW_CONTENTS_MARGIN);
+    widget->setParent(m_root);
+    m_layout->addWidget(widget, widgetRow, 0, 1, 2);
+
+    // stretch:
+    const int stretchRow(widgetRow + 1);
+    m_layout->setRowStretch(stretchRow, 10);
 }
 
+void NewTargetWindow::appendSubWidget(QGridLayout *layout, QWidget *widget,
+                                      const QString &title, const QString &subTitle)
+{
+    int insertionRow = layout->rowCount();
+    if (!title.isEmpty()) {
+        QLabel *label = new QLabel;
+        label->setText(title);
+        QFont f = label->font();
+        f.setBold(true);
+        f.setPointSizeF(f.pointSizeF() * 1.2);
+        label->setFont(f);
+        label->setContentsMargins(0, 10, 0, 0);
+
+        layout->addWidget(label, insertionRow, 0, 1, 2);
+        layout->setRowStretch(insertionRow, 0);
+        ++insertionRow;
+
+        if (!subTitle.isEmpty()) {
+            QLabel *subLabel = new QLabel;
+            subLabel->setText(subTitle);
+            subLabel->setWordWrap(true);
+            QFont smallFont = subLabel->font();
+            smallFont.setPointSizeF(smallFont.pointSizeF() / 1.2);
+            subLabel->setFont(smallFont);
+
+            layout->addWidget(subLabel, insertionRow, 0, 1, 2);
+            layout->setRowStretch(insertionRow, 0);
+            ++insertionRow;
+        }
+
+        if (widget) {
+            widget->setContentsMargins(0, 10, 0, 0);
+            layout->addWidget(widget, insertionRow, 1, 1, 1);
+        }
+    } else if (widget)
+        layout->addWidget(widget, insertionRow, 0, 1, 2);
+}
+
+} // namespace Internal
+} // namespace Inspector
